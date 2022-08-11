3b984dcdfaad94cbab82ed28ec452c146c8f9ff2
Quang Hà <btkkentxx2002@gmail.com>
2022-08-11 16:44:49 +0700

    jv

---
+++ src/LinearEquationResolver.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0', pleas ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+
+        if (a != 0){
+
+        }
+
+
+    }
+}
